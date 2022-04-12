1e01209858379cb45ec15d44c3cdf2d6342a284c
Nsikakabasi Samuel John <Jsammyn@gmail.com>
2022-04-12 14:24:22 -0700

    3-islower.c Task 3 repaired 2

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,14 +1,22 @@
 #include "main.h"
 
 /**
- * _isalpha - check for lower case letter
- * @c : character to check
- * Return:0 or 1
+ * _islower - Short description, single line
+ * @c: contains value to be compared
+ * Return: Always 0.
  */
+int _islower(int c)
 
-int _isalpha(int c)
 {
-	return ((c >= 97 && c <= 122) || (c >= 65 && c <= 90));
 
+if (c > 'a' && c < 'z')
+{
+return (1);
 }
 
+else
+{
+return (0);
+}
+
+}
